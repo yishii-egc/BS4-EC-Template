8b6490adab9c836b79f7a28806ece0a2d71f8000
yishii <ishii@evergreen-creator.com>
2019-02-19 18:24:09 +0900

    TOPページの制作（0219）

--- common/js/common.js
+++ common/js/common.js
@@ -22,3 +22,41 @@ $(window).on('load resize', function(){
   var currentmenu = $('.categorynav_scroll_x ul li .active').offset();//.active要素のoffsetを取得
   console.log(currentmenu);
 });
+
+// TOPに戻るボタン
+  $(function($) {
+    var $toTopBtn = $(['<div id="toTopBtn" title="ページTOPへ">',
+                       '  <i class="fas fa-chevron-up"></i>',
+                       '</div>'].join(''));
+    $toTopBtn.on('click', function () {
+      $('body, html').animate({
+              scrollTop: 0
+      }, 500);
+      return false;
+    });
+
+    $('body > footer')
+      .before($toTopBtn.hide());
+
+    $(window).on('scroll', function () {
+      if (50 < $(this).scrollTop()) {
+              $toTopBtn.fadeIn();
+      } else {
+              $toTopBtn.fadeOut();
+      }
+    });
+  });
+
+// フッターの電話番号リンク（スマホ時のみ作動）
+if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)) {
+  $(function() {
+    $('.tel').each(function() {
+      var str = $(this).html();
+      if ($(this).children().is('img')) {
+        $(this).html($('<a>').attr('href', 'tel:' + $(this).children().attr('alt').replace(/-/g, '')).append(str + '</a>'));
+      } else {
+        $(this).html($('<a>').attr('href', 'tel:' + $(this).text().replace(/-/g, '')).append(str + '</a>'));
+      }
+    });
+  });
+}
