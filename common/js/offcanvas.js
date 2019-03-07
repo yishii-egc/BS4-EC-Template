90a52040b8d10bdd2baef2a30832e4b546806a57
yishii <ishii@evergreen-creator.com>
2019-03-07 18:48:21 +0900

    メディカルガイドカテゴリーページ追加

--- common/js/offcanvas.js
+++ common/js/offcanvas.js
@@ -10,22 +10,3 @@ $(function () {
     $('.offcanvas_item').toggleClass('open')
   })
 });
-$(function () {
-  var timer = false;
-  $(window).scroll(function() {
-      if (timer !== false) {
-          clearTimeout(timer);
-      }
-      timer = setTimeout(function() {
-        var navheight = $('.navbar_main').height();
-        var t = $('.delivery_info').offset().top + 10; // ターゲットの位置取得
-        var s = t - navheight ;
-        console.log(navheight);
-        if($(window).scrollTop() > t){
-          $('.slide_cart').addClass('slidein'); // ボタンを非表示
-        }else{
-          $('.slide_cart').removeClass('slidein'); // ボタンを表示
-        }
-      }, 5);
-  });
-});
--- js/item.js
+++ js/item.js
@@ -7,4 +7,41 @@ $(function(){
   });
 });
 
-// 商品購入スライドの出し入れ
+// リロード時、上部のカート表がスクロール外になってたらスライドカートを表示する
+  $(function () {
+    var navheight = $('.navbar_main').height();
+    var t = $('.delivery_info').offset().top + 10; // ターゲットの位置取得
+    var s = t - navheight ;
+    console.log(navheight);
+    if($(window).scrollTop() > t){
+      $('.slide_cart').addClass('slidein');
+    }else{
+      $('.slide_cart').removeClass('slidein');
+    }
+  });
+
+// スクロールして上部のカート表がスクロール外になってたらスライドカートを表示する
+  $(function () {
+    var timer = false;
+    $(window).scroll(function() {
+        if (timer !== false) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(function() {
+          var navheight = $('.navbar_main').height();
+          var t = $('.delivery_info').offset().top + 10; // ターゲットの位置取得
+          var s = t - navheight ;
+          console.log(navheight);
+          if($(window).scrollTop() > t){
+            $('.slide_cart').addClass('slidein'); // ボタンを非表示
+          }else{
+            $('.slide_cart').removeClass('slidein'); // ボタンを表示
+          }
+        }, 5);
+    });
+  });
+
+// スライドカートの×をクリックしたらスライドカートを引っ込める
+  $('.slide_cart_close').click(function(){
+      $('.slide_cart').removeClass('slidein');
+  });
