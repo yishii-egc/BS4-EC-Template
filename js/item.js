c56b8480fd9c1435257ea1d9afc07571a3472037
yishii <ishii@evergreen-creator.com>
2019-03-13 13:39:33 +0900

    商品ページの商品情報をh2ごとに表示切替方式に

--- js/item.js
+++ js/item.js
@@ -7,7 +7,7 @@ $(function(){
   });
 });
 
-// リロード時、上部のカート表がスクロール外になってたらスライドカートを表示する
+// リロード時、上部のカート表が画面の外になってたらスライドカートを表示する
   $(function () {
     var navheight = $('.navbar_main').height();
     var t = $('.delivery_info').offset().top + 10; // ターゲットの位置取得
@@ -33,9 +33,11 @@ $(function(){
           var s = t - navheight ;
           console.log(navheight);
           if($(window).scrollTop() > t){
-            $('.slide_cart').addClass('slidein'); // ボタンを非表示
-          }else{
-            $('.slide_cart').removeClass('slidein'); // ボタンを表示
+            $('.slide_cart').addClass('slidein'); // カートをスライドイン
+          } else if(){
+            
+          } else{
+            $('.slide_cart').removeClass('slidein'); // カートをスライドアウト
           }
         }, 5);
     });
@@ -45,3 +47,36 @@ $(function(){
   $('.slide_cart_close').click(function(){
       $('.slide_cart').removeClass('slidein');
   });
+
+$(function(){
+  $('.slick_wrapper').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev btn btn-small btn_white_trans"><i class="fas fa-angle-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next btn btn-small btn_white_trans"><i class="fas fa-angle-right"></i></button>',
+    responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+    ]
+  });
+});
--- js/medical_guide_single.js
+++ js/medical_guide_single.js
@@ -9,3 +9,16 @@ $(window).on('load resize', function(){
       $('.medical_toc > .list-group').css('margin-top','0');
     }
 });
+
+// スムーススクロール
+  $(function(){
+    $('a[href^="#"]').click(function(){
+      var speed = 200;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var headerHeight = $('.navbar_main').outerHeight(); 
+      var position = target.offset().top - headerHeight - 16;
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
+    });
+  });
