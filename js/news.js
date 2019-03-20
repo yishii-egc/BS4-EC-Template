ed38a559af5a5d3560ce0ac4c53ea5a8b814efd2
yishii <ishii@evergreen-creator.com>
2019-03-20 15:21:56 +0900

    ニュースページのJSを外部ファイル化

---
+++ js/news.js
@@ -0,0 +1,44 @@
+// 画面遷移時のURLにニュースIDのハッシュタグが付いてたらそのニュースを開く
+  $(function(){
+    if(location.hash){
+        var hash = location.hash;
+        $(hash).find('.collapse').toggleClass('show');
+    }
+  });
+
+// スムーススクロール
+  $(function(){
+     //URLのハッシュ値を取得
+  var urlHash = location.hash;
+  var headHeight = $('.navbar_main').outerHeight();
+  //ハッシュ値があればページ内スクロール
+  if(urlHash) {
+    //スクロールを0に戻す
+    $('body,html').stop().scrollTop(0) + headHeight;
+    setTimeout(function () {
+      //ロード時の処理を待ち、時間差でスクロール実行
+      scrollToAnker(urlHash) ;
+    }, 100);
+  }
+
+//通常のクリック時
+  $('a[href^="#"]').click(function() {
+    //ページ内リンク先を取得
+    var href= $(this).attr("href");
+    //リンク先が#か空だったらhtmlに
+    var hash = href == "#" || href == "" ? 'html' : href;
+    //スクロール実行
+    scrollToAnker(hash);
+    //リンク無効化
+    return false;
+  });
+
+  // 関数：スムーススクロール
+  // 指定したアンカー(#ID)へアニメーションでスクロール
+  function scrollToAnker(hash) {
+      var target = $(hash);
+      var headerHeight = $('.navbar_main').outerHeight();
+      var position = target.offset().top - headerHeight - 16;
+      $('body,html').stop().animate({scrollTop:position}, 500);
+    }
+  })
