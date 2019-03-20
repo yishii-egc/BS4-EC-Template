42f16d0fe8a5446720520e64db95491304710831
yishii <ishii@evergreen-creator.com>
2019-03-20 17:34:05 +0900

    お問い合わせページを追加 各ページのヘッダー＆フッターからお問い合わせページへのリンク追加

--- common/js/common.js
+++ common/js/common.js
@@ -49,16 +49,3 @@ if (navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)) {
     });
   });
 }
-
-// スムーススクロール
-//  $(function(){
-//    $('a[href^="#"]').click(function(){
-//      var speed = 200;
-//      var href= $(this).attr("href");
-//      var target = $(href == "#" || href == "" ? 'html' : href);
-//      var headerHeight = $('.navbar_main').outerHeight(); 
-//      var position = target.offset().top - headerHeight - 16;
-//      $("html, body").animate({scrollTop:position}, speed, "swing");
-//      return false;
-//    });
-//  });
