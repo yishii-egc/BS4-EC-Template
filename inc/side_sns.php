547d9bc9c94c477eb6798ba34b9a80b529cb9828
yishii <ishii@evergreen-creator.com>
2019-04-03 17:56:55 +0900

    サイドバーのSNSを外部ファイル化

--- inc/side_sns.php
+++ inc/side_sns.php
@@ -1,8 +1,8 @@
         <section class="side_sns">
           <h5>お薬通販部公式SNS</h5>
           <ul class="link_sns list-unstyled">
-            <li class="mb-2"><a href="https://www.facebook.com/okusurishop0/" class="btn btn-block btn_facebook" rel="nofollow" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-            <li class="mb-2"><a href="https://twitter.com/okusuri_shop" class="btn btn-block btn_twitter" rel="nofollow" target="_blank"><i class="fab fa-twitter"></i></a></li>
-            <li class="mb-2"><a href="https://www.instagram.com/okusuri_shop/" class="btn btn-block btn_instagram" rel="nofollow" target="_blank"><i class="fab fa-instagram"></i></a></li>
+            <li class="mb-2 px-2"><a href="https://www.facebook.com/okusurishop0/" class="btn btn-block btn_facebook" rel="nofollow" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+            <li class="mb-2 px-2"><a href="https://twitter.com/okusuri_shop" class="btn btn-block btn_twitter" rel="nofollow" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li class="mb-2 px-2"><a href="https://www.instagram.com/okusuri_shop/" class="btn btn-block btn_instagram" rel="nofollow" target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </section>
