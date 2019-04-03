be587acc1c77f3269b21fbfe3d868b923453a3cd
yishii <ishii@evergreen-creator.com>
2019-04-03 18:19:54 +0900

    サイドのSNSリンクを修正

--- inc/side_sns.php
+++ inc/side_sns.php
@@ -1,8 +1,9 @@
-        <section class="side_sns">
+        <section class="side_sns mb-4">
           <h5>お薬通販部公式SNS</h5>
-          <ul class="link_sns list-unstyled">
-            <li class="mb-2 px-2"><a href="https://www.facebook.com/okusurishop0/" class="btn btn-block btn_facebook" rel="nofollow" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-            <li class="mb-2 px-2"><a href="https://twitter.com/okusuri_shop" class="btn btn-block btn_twitter" rel="nofollow" target="_blank"><i class="fab fa-twitter"></i></a></li>
-            <li class="mb-2 px-2"><a href="https://www.instagram.com/okusuri_shop/" class="btn btn-block btn_instagram" rel="nofollow" target="_blank"><i class="fab fa-instagram"></i></a></li>
+          <ul class="link_sns list-unstyled mb-0 row px-3">
+            <li class="mb-2 col-4 col-md-12"><a href="https://www.facebook.com/okusurishop0/" class="btn btn-block btn_facebook" rel="nofollow" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+            <li class="mb-2 col-4 col-md-12"><a href="https://twitter.com/okusuri_shop" class="btn btn-block btn_twitter" rel="nofollow" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li class="mb-2 col-4 col-md-12"><a href="https://www.instagram.com/okusuri_shop/" class="btn btn-block btn_instagram" rel="nofollow" target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
+          <p class="mx-3 mb-0 small">SNS限定クーポン発行などお得な情報を発信しています。</p>
         </section>
